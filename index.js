91c3c5b90739fa6f14e38bab49e88c5ebbc4c8ed
Festus Wanjohi <strategicpolicyafrica@gmail.com>
2022-09-01 10:02:38 +0300

    Layout progress

---
+++ index.js
@@ -0,0 +1,3 @@
+const shoeImg = document.getElementById("shoeimage1");
+shoeImg.style.backgroundImage =
+  'url("https://images.asos-media.com/products/fred-perry-hughes-hi-top-leather-sneakers-in-black/203433481-1-black")';
